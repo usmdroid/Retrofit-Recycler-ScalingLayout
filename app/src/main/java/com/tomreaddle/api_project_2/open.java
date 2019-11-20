c423712351eab80b248a9affaeff0041d5d4aa3f
Usmanov8102 <elusm17@gmail.com>
2019-11-20 21:53:19 +0500

    API JSON Retrofit2 Glide RecyclerView Design ScalingLayout Intent Extra datas

--- app/src/main/java/com/tomreaddle/api_project_2/open.java
+++ app/src/main/java/com/tomreaddle/api_project_2/open.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,6 +44,12 @@ public class open extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         open_title.setText(name);
         open_createdby.setText(createdby);
